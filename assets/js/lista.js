f557d67081774c8e0b1f7319706893e09cefbb7a
ana julia <anamondinip@gmail.com>
2023-05-30 15:34:57 -0300

    ana

--- assets/js/lista.js
+++ assets/js/lista.js
@@ -1,24 +1,25 @@
 var array = [];
-var array1 = [];
+var array1 = []; 
 
 function lista(){
     let titulo = document.getElementById("inputtitulo").value;
     let descrição = document.getElementById("Descriçaoinput").value;
     let autor = document.getElementById("autorinput").value;
     let data = document.getElementById("Datainput").value;
-    array.push(titulo);
-    array.push(descrição);
-    array.push(autor);
-    document.getElementById("inputtitulo").value = '';
-    document.getElementById("Descriçaoinput").value = '';
-    document.getElementById("autorinput").value = '';
-    document.getElementById("Datainput").value = '';
-    let msg = '';
-    for (let i = 0; i < array.length; i++){
-        msg += `<p>${array[i]}</p>`
-    }
+    array1.push(titulo);
+    array1.push(descrição);
+    array1.push(autor);
+    array1.push(data);
+
+    array.push(array1); 
+
     document.getElementById("p1").innerHTML = `${titulo}`;
     document.getElementById("p2").innerHTML = `Data de Publicação: ${data}`;
     document.getElementById("p3").innerHTML = `Descrição: ${descrição}`;
     document.getElementById("p4").innerHTML = `Autor: ${autor}`;
+    
+    
 }
+
+
+
